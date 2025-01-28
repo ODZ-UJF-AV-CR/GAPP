c8827ec8cab5d42eac4daffa35013ebdff4e4939
Vilém Raška <wilhelm.master@gmail.com>
2025-01-28 22:23:22 +0100

    ui: update menu bar

--- apps/gapp-dashboard/src/components/menu/menu.component.ts
+++ apps/gapp-dashboard/src/components/menu/menu.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+import { NgIcon, provideIcons, provideNgIconsConfig } from '@ng-icons/core';
+import { tablerHome, tablerSettings } from '@ng-icons/tabler-icons';
 
 @Component({
     selector: 'gapp-menu',
     templateUrl: './menu.component.html',
-    imports: [RouterLink],
+    imports: [RouterLink, NgIcon, RouterLinkActive],
+    providers: [provideIcons({ tablerHome, tablerSettings }), provideNgIconsConfig({ size: '1.5rem' })],
 })
 export class MenuComponent {}
---
+++ apps/gapp-dashboard/src/services/theme.service.ts
@@ -0,0 +1,6 @@
+import { Injectable } from '@angular/core';
+
+export type Theme = 'light' | 'dark';
+
+@Injectable({ providedIn: 'root' })
+export class themeService {}

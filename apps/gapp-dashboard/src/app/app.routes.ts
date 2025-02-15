3ed4e5a6ea2856e7f8dd432ccbc28cc4a93d6148
Vilém Raška <v.rashka.ext@make.com>
2025-02-15 20:52:21 +0100

    feat: update titles

--- apps/gapp-dashboard/src/app/app.routes.ts
+++ apps/gapp-dashboard/src/app/app.routes.ts
@@ -8,18 +8,22 @@ export const appRoutes: Route[] = [
     {
         path: 'cars',
         loadComponent: () => import('@/pages/cars/cars.component').then((c) => c.CarsComponent),
+        title: 'GAPP | Cars'
     },
     {
         path: 'vessels',
         loadComponent: () => import('@/pages/vessels/vessels.component').then((c) => c.VesselsComponent),
+        title: 'GAPP | Vessels'
     },
     {
         path: 'map',
         loadComponent: () => import('@/pages/map/map.component').then((c) => c.MapComponent),
+        title: 'GAPP | Map'
     },
     {
         path: 'dashboard',
         loadComponent: () => import('@/pages/dashboard/dashboard.component').then((c) => c.DashboardComponent),
+        title: 'GAPP | Dashboard'
     },
     {
         path: '**',

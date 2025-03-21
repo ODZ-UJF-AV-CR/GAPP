4972354680ec84029c7fe55d51b4918f39adf05d
Vilém Raška <v.rashka.ext@make.com>
2025-03-21 18:21:52 +0100

    feat: update dashboard design

--- apps/gapp-dashboard/src/components/navbar/navbar.component.ts
+++ apps/gapp-dashboard/src/components/navbar/navbar.component.ts
@@ -1,14 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { NgIcon, provideIcons, provideNgIconsConfig } from '@ng-icons/core';
-import { tablerHome, tablerLayoutDashboard, tablerAdjustments, tablerRouter, tablerListDetails, tablerMapPins } from '@ng-icons/tabler-icons';
+import {
+    tablerHome,
+    tablerLayoutDashboard,
+    tablerAdjustments,
+    tablerRouter,
+    tablerListDetails,
+    tablerMapPins,
+    tablerWifi,
+    tablerBuildingBroadcastTower,
+} from '@ng-icons/tabler-icons';
 
 @Component({
     selector: 'gapp-navbar',
     templateUrl: './navbar.component.html',
     imports: [RouterLink, NgIcon, RouterLinkActive],
     providers: [
-        provideIcons({ tablerHome, tablerLayoutDashboard, tablerAdjustments, tablerRouter, tablerListDetails, tablerMapPins }),
+        provideIcons({
+            tablerHome,
+            tablerLayoutDashboard,
+            tablerAdjustments,
+            tablerRouter,
+            tablerListDetails,
+            tablerMapPins,
+            tablerWifi,
+            tablerBuildingBroadcastTower,
+        }),
         provideNgIconsConfig({ size: '1.5rem' }),
     ],
 })
--- apps/gapp-dashboard/src/pages/dashboard/dashboard.component.ts
+++ apps/gapp-dashboard/src/pages/dashboard/dashboard.component.ts
@@ -1,8 +1,8 @@
-import { ChangeDetectionStrategy, Component, computed, inject, OnDestroy, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnDestroy, OnInit } from '@angular/core';
 import { DashboardService } from './dashboard.service';
 import { PageBlockComponent } from '@/components/page-block/page-block.component';
 import { ScrollableComponent } from '@gapp/ui/scrollable';
-import { VehicleService, VehicleType } from '@/services/vehicle.service';
+import { VehicleService } from '@/services/vehicle.service';
 import { TelemetryCardComponent } from './telemetry-card/telemetry-card.component';
 
 @Component({
@@ -15,8 +15,7 @@ export class DashboardComponent implements OnInit, OnDestroy {
     private dashboardService = inject(DashboardService);
     private vehicleService = inject(VehicleService);
 
-    public groundVehicles = computed(() => this.dashboardService.vehiclesWithTelemetry().filter((v) => v.type === VehicleType.CAR));
-    public airVehicles = computed(() => this.dashboardService.vehiclesWithTelemetry().filter((v) => v.type !== VehicleType.CAR));
+    public telemetry = this.dashboardService.vehiclesWithTelemetry;
 
     public ngOnInit() {
         this.vehicleService.loadVehicles();

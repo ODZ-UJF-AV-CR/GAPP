5e4324cef51e9f5b49f8aed10aebfa2a2e988d3e
Vilém Raška <v.rashka.ext@make.com>
2026-02-05 23:10:35 +0100

    migrate to workspaces

---
+++ packages/ui/projects/forms/src/lib/forms.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'lib-forms',
+    imports: [],
+    template: ` <p>forms works!</p> `,
+    styles: ``,
+})
+export class Forms {}

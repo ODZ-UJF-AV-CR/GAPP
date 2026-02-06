9859929c90ed179d5cd548ffa4b2c5269ec99e41
Vilém Raška <v.rashka.ext@make.com>
2026-02-06 09:59:24 +0100

    wip

--- apps/dashboard/src/app/app.ts
+++ apps/dashboard/src/app/app.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { Forms } from '@gapp/ui';
+import { Forms } from '@gapp/ui/forms';
 
 @Component({
     selector: 'app-root',

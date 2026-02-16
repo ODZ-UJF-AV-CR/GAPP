d61b356b9b317e92fb27f386ed21cfdf8ee418b9
Vilém Raška <v.rashka.ext@make.com>
2026-02-16 08:45:53 +0100

    wip

--- apps/dashboard/src/app/core/components/navbar/navbar.component.ts
+++ apps/dashboard/src/app/core/components/navbar/navbar.component.ts
@@ -1,36 +1,13 @@
-import { JsonPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
-import { GappRoute, type GappRoutes } from '@app/app.routes';
-import { NgIcon, provideIcons, provideNgIconsConfig } from '@ng-icons/core';
-import {
-    tablerAdjustments,
-    tablerBuildingBroadcastTower,
-    tablerHome,
-    tablerLayoutDashboard,
-    tablerListDetails,
-    tablerMapPins,
-    tablerRouter,
-    tablerWifi,
-} from '@ng-icons/tabler-icons';
+import type { GappRoutes } from '@app/app.routes';
+import { NgIcon, provideNgIconsConfig } from '@ng-icons/core';
 
 @Component({
     selector: 'gapp-navbar',
     templateUrl: './navbar.component.html',
     imports: [RouterLink, NgIcon, RouterLinkActive],
-    providers: [
-        provideIcons({
-            tablerHome,
-            tablerLayoutDashboard,
-            tablerAdjustments,
-            tablerRouter,
-            tablerListDetails,
-            tablerMapPins,
-            tablerWifi,
-            tablerBuildingBroadcastTower,
-        }),
-        provideNgIconsConfig({ size: '1.5rem' }),
-    ],
+    providers: [provideNgIconsConfig({ size: '1.5rem' })],
 })
 export class NavbarComponent {
     private router = inject(Router);

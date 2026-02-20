1025603e590e3222036649fe0763a3f4ba9fb83d
Vilém Raška <v.rashka.ext@make.com>
2026-02-20 22:37:34 +0100

    wip: layout

--- apps/dashboard/src/app/features/telemetry/telemetry-dashboard.component.ts
+++ apps/dashboard/src/app/features/telemetry/telemetry-dashboard.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
 import { HeaderContentDirective } from '@core/components/header/header-content.directive';
-import { CenterLayoutComponent, SimpleSectionComponent, TripleSectionComponent } from '@shared/layout';
 
 @Component({
     selector: 'telemetry-dashboard',
     templateUrl: './telemetry-dashboard.component.html',
-    imports: [TripleSectionComponent, CenterLayoutComponent, SimpleSectionComponent, HeaderContentDirective],
+    imports: [HeaderContentDirective],
 })
 export class TelemetryDashboardComponent {
     public text = 'asdasdadsdsa';

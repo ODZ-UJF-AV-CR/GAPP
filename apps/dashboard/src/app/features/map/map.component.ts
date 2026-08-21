9e4db37bc1d5cd9ac8f93550577b03eb1414d1b7
Vilém Raška <v.rashka.ext@make.com>
2026-08-21 20:44:46 +0200

    wip: map

--- apps/dashboard/src/app/features/map/map.component.ts
+++ apps/dashboard/src/app/features/map/map.component.ts
@@ -1,5 +1,5 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
-import { GappMapComponent } from '@shared/components/gapp-map';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
+import { GappMapComponent, type SymbolLayer } from '@shared/components/gapp-map';
 
 @Component({
     selector: 'map',
@@ -7,4 +7,27 @@ import { GappMapComponent } from '@shared/components/gapp-map';
     changeDetection: ChangeDetectionStrategy.OnPush,
     imports: [GappMapComponent],
 })
-export class MapComponent {}
+export class MapComponent {
+    public readonly layer: SymbolLayer = {
+        layerId: 'baloon-layer',
+        type: 'symbol',
+        layout: {
+            'icon-image': 'chase-car',
+            'icon-size': 0.5,
+            'icon-overlap': 'always',
+        },
+        data: signal({
+            type: 'FeatureCollection',
+            features: [
+                {
+                    type: 'Feature',
+                    properties: {},
+                    geometry: {
+                        type: 'Point',
+                        coordinates: [14, 50],
+                    },
+                },
+            ],
+        }),
+    };
+}
